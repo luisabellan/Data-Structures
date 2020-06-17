5297a63aab9372510b0f0c7f841d68e08c3d6699
Luis Abellan <luis.abellan.dev@gmail.com>
2020-06-17 04:06:29 +0200

    added comment

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,7 @@
+#Just a FYI: If you use the code from today's
+# lecture for the singly-linked-list,
+# sticking it in the singly_linked_list.py file,
+# you will need to add a get_max() method
+# if you want all of the tests to pass in
+# test_singly_linked_list.py.
+# Not a big deal at all; just thought I'd mention
