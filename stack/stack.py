85b9bd8d486d6eb96e20f258635e5c3923723494
Luis Abellan <luis.abellan.dev@gmail.com>
2020-06-18 03:37:15 +0200

    stack done

--- stack/stack.py
+++ stack/stack.py
@@ -1,3 +1,4 @@
+# pylint: disable=no-member
 """
 A stack is a data structure whose primary purpose is to store and
 return elements in Last In First Out order. 
@@ -13,13 +14,49 @@ return elements in Last In First Out order.
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
 
     def __len__(self):
-        pass
+        
+        return self.size
 
     def push(self, value):
-        pass
+        self.value = value
+        self.size += 1
+        
+        return self.storage.insert(0, self.value)
 
     def pop(self):
-        pass
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.pop(0)
+
+""" 
+stack = Stack()
+print(f"list: {stack.storage}")
+print(f"Length of the stack: {len(stack)}")
+print(f"we add the value to 2 to the stack")
+print(f"list: {stack.storage}")
+stack.push(2)
+print(f"we add the value to 5 to the stack")
+stack.push(5)
+print(f"list: {stack.storage}")
+print(f"we add the value to 7 to the stack")
+stack.push(7)
+print(f"list: {stack.storage}")
+print(f"Length of the stack: {len(stack)}")
+print(f"Size: {stack.size}")
+print(f"we pop to the stack")
+stack.pop()
+print(f"list: {stack.storage}")
+print(f"Length of the stack: {len(stack)}")
+print(f"Size: {stack.size}")
+stack.pop()
+print(f"list: {stack.storage}")
+print(f"Length of the stack: {len(stack)}")
+print(f"Size: {stack.size}")
+stack.pop()
+print(f"list: {stack.storage}")
+print(f"Length of the stack: {len(stack)}")
+print(f"Size: {stack.size}")
+ """
